9f3dd40ae3cabbc4443be6ade197005663d77555
Priyankaa2503 <priyankaar25@gmail.com>
2024-01-31 20:41:35 +0530

    uiiii

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,47 +5,61 @@ export default function Header() {
   const user = JSON.parse(localStorage.getItem('user'));
   return (
     <header className="flex justify-between items-center w-full border-b py-6 sm:px-4 px-2 border-gray-500 gap-2">
-      <Link to="/" className="flex space-x-2">
-        <img alt="header text" src="/bed.svg" className="sm:w-10 sm:h-10 w-9 h-9" width={24} height={24} />
-        <h1 className="sm:text-3xl text-xl font-bold ml-2 tracking-tight">DREAM HOUSE</h1>
+      <Link to="/" className="flex space-x-2 items-center gap-2">
+        <img
+          alt="header text"
+          src="/House_icon.png"
+          className="sm:w-10 sm:h-10 w-9 h-9"
+          width={24}
+          height={24}
+        />
+        <h1 className="sm:text-3xl text-xl font-light ml-2 tracking-tight ">
+          Dinter
+        </h1>
       </Link>
       {token ? (
         <div className="flex items-center space-x-4">
           <Link
             to="/dashboard"
-            className="border-r border-gray-300 pr-4 flex space-x-2 hover:text-purple-400 transition"
+            className=" border-gray-300 pr-4 flex space-x-2 hover:text-[#01BD57] transition"
           >
             <div>Dashboard</div>
           </Link>
           <Link
             to="/progress"
-            className="border-r border-gray-300 pr-4 flex space-x-2 hover:text-purple-400 transition"
+            className=" border-gray-300 pr-4 flex space-x-2 hover:text-[#01BD57] transition"
           >
             <div>Progress</div>
           </Link>
-        
-          <Link to="/compare" className="border-r border-gray-300 pr-4 flex space-x-2 hover:text-purple-400 transition">
+
+          <Link
+            to="/compare"
+            className="border-gray-300 pr-4 flex space-x-2 hover:text-[#01BD57] transition"
+          >
             <div>Compare</div>
           </Link>
           <Link
             to="/paint-cost"
-            className="border-r border-gray-300 pr-4 flex space-x-2 hover:text-purple-400 transition"
+            className="border-gray-300 pr-4 flex space-x-2 hover:text-[#01BD57] transition"
           >
             <div>Cost Estimation</div>
           </Link>
           <Link
             to="https://64323ff406fb264b1328e217--poetic-lokum-8589b1.netlify.app/"
-            className="border-r border-gray-300 pr-4 flex space-x-2 hover:text-purple-400 transition"
+            className="border-gray-300 pr-4 flex space-x-2 hover:text-[#01BD57] transition"
           >
             <div>Playground</div>
           </Link>
           <Link
             to="/explore"
-            className="border-r border-gray-300 pr-4 flex space-x-2 hover:text-purple-400 transition"
+            className="border-gray-300 pr-4 flex space-x-2 hover:text-[#01BD57] transition"
           >
             <div>Explore</div>
           </Link>
-          <Link className="flex items-center gap-2 hover:text-purple-400 transition" to="/profile">
+          <Link
+            className="flex items-center gap-2 hover:text-purple-400 transition"
+            to="/profile"
+          >
             <h1 className="font-semibold">{user.name}</h1>
             <img
               alt="Profile picture"
@@ -59,13 +73,13 @@ export default function Header() {
       ) : (
         <div className="flex gap-4">
           <Link
-            className="flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-purple-600 text-white px-5 py-2 text-sm shadow-md hover:bg-purple-400 bg-purple-600 font-medium transition"
+            className="flex max-w-fit items-center justify-center space-x-2 rounded-lg border text-white px-5 py-2 text-sm shadow-md font-medium transition"
             to="/login"
           >
             <p>Login</p>
           </Link>
           <Link
-            className="flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-purple-600 text-white px-5 py-2 text-sm shadow-md hover:bg-purple-400 bg-purple-600 font-medium transition"
+            className="flex max-w-fit items-center justify-center space-x-2 rounded-lg border  text-white px-5 py-2 text-sm shadow-md  font-medium transition"
             to="/signup"
           >
             <p>Signup</p>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import Header from '../components/Header';
-import SquigglyLines from '../components/SquigglyLines';
-import Footer from '../components/Footer';
 import {image} from '../constant'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
 
 const HomePage = () => {
   console.log(image);
   return (
     <div className="flex w-full flex-col items-center justify-start py-2 min-h-screen">
       <Header />
+
       <main className="flex flex-1 w-full flex-col items-start justify-start  sm:mt-20 mt-20 background-gradient">
         <div className="flex">
           <div className="">
@@ -22,51 +23,41 @@ const HomePage = () => {
             </h1>
             {/* <div className="flex items-center justify-center my-28 rounded-lg"> */}
 
-            <h2 className="mx-auto mt-12 max-w-xl text-xl sm:text-gray-200  text-gray-200 leading-7">
+            <h2 className="mx-auto mt-12 max-w-xl text-xl sm:text-gray-300  text-gray-300 leading-7">
               The perfect balance between inner guiding you while still bringing
               out your vision essence of you.
             </h2>
           </div>
           <img src="Choosing.svg" alt="" className="w-[40%] h-[40%] ml-20" />
         </div>
-        <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
+        <div className="flex justify-between items-center w-full flex-col sm:mt-20 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
-            <h1 className="mx-auto mb-4 max-w-4xl font-display text-4xl font-bold tracking-normal text-gray-300 sm:text-6xl">
-              Look at our samples.
-            </h1>
-            <div className="flex sm:space-x-8 sm:flex-row flex-col">
+            <p className="mx-auto mb-4 max-w-4xl font-display text-4xl font-normal tracking-normal text-gray-300 sm:text-6xl">
+              Explore our interior designs
+            </p>
+            <Carousel>
               <div>
-                <h3 className="mb-1 font-medium text-lg">Original Room</h3>
-                <img
-                  alt="Original photo of a room with roomGPT.io"
-                  src="/1.jpg"
-                  className="w-full object-cover h-96 rounded-2xl"
-                  width={400}
-                  height={400}
-                />
+                <img src="https://assets-global.website-files.com/64268d580a10a12e64fd4ab7/642eb36e53b7ee8a9e437dce_pr-06.jpg" />
               </div>
-              <div className="sm:mt-0 mt-8">
-                <h3 className="mb-1 font-medium text-lg">Generated Room</h3>
-                <img
-                  alt="Generated photo of a room with roomGPT.io"
-                  width={400}
-                  height={400}
-                  src="/1-new.jpg"
-                  className="w-full object-cover h-96 rounded-2xl sm:mt-0 mt-2"
-                />
+              <div>
+                <img src="https://assets-global.website-files.com/64268d580a10a12e64fd4ab7/642eb36e53b7ee8a9e437dce_pr-06.jpg" />
               </div>
+              <div>
+                <img src="https://assets-global.website-files.com/64268d580a10a12e64fd4ab7/642eb3e1eba5745fcf9b12f9_pr-09.jpg" />
+              </div>
+            </Carousel>
+            <div className="flex justify-center">
+              <Link
+                className="bg-[#01BD57] w-[300px] text-center text-xl rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-purple-500 transition"
+                to="/login"
+              >
+               Get Onboard with us!
+              </Link>
             </div>
-            <Link
-              className="bg-purple-600 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-purple-500 transition"
-              to="/login"
-            >
-              Renovate you room
-            </Link>
           </div>
         </div>
       </main>
       {/* <img src={image} alt="" /> */}
-      <Footer />
     </div>
   );
 };
